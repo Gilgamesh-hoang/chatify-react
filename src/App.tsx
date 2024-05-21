be4ad17fb9461173d4fd8546b69416a0437f45b2
cuong-do-viet <21130299@st.hcmuaf.edu.vn>
2024-05-21 21:00:59 +0700

    Router configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import {publicRoutes, RouteType} from "./router";
+import DefaultLayout from "./layout/DefaultLayout";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
-  return (
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1>
-  );
+    const RouteRender = (route: RouteType, index: number) => {
+        let Layout = route.layout || DefaultLayout
+        let Page = route.element
+        return (
+            <Route key={index} path={route.path} element={
+                <Layout>
+                    <Page/>
+                </Layout>
+            }/>
+        )
+    };
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-600">
+                Simple React Typescript Tailwind Sample
+            </h1>
+            <Router>
+                <Routes>
+                    {
+                        publicRoutes.map((routeObject, index: number) =>
+                            RouteRender(routeObject, index)
+                        )
+                    }
+                    <Route path='*' element={null}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
