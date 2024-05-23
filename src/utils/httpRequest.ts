9b7a14f3a5f929541235491e4e834c93d3f15928
Gilgamesh-hoang <vophihoang252003@gmail.com>
2024-05-23 17:16:40 +0700

    config: axios lib

---
+++ src/utils/httpRequest.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const baseURL = process.env.REACT_APP_BACKEND_URL;
+
+if (!baseURL) {
+    throw new Error('REACT_APP_BACKEND_URL is not defined in .env file');
+}
+
+const httpRequest: AxiosInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 5000,
+});
+
+export const get = async <T>(path: string, option: AxiosRequestConfig = {}): Promise<T> => {
+    const response: AxiosResponse<T> = await httpRequest.get<T>(path, option);
+    return response.data;
+}
+
+export default httpRequest;
