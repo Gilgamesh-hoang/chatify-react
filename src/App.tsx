5de1bd8bdaf8de4ce241b15c982783f5964080df
hoanghuycoder <161137978+hoanghuycoder@users.noreply.github.com>
2024-06-13 21:02:21 +0700

    fix : resolve conflict & config socket to send msg (#16)
    * refactor : auth page
    
    * refactor : socket
    
    * config : config socket to send message
    
    ---------
    
    Co-authored-by: hoanghuydev <tranvohoanghuy12ab@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,86 @@
-import React, {useEffect, useLayoutEffect} from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import './App.css';
-import {privateRoutes, publicRoutes, RouteType} from "./router";
-import DefaultLayout from "./layout/DefaultLayout";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {useDispatch, useSelector} from 'react-redux';
-import {socketSelector, userSelector} from './redux/selector';
+import { privateRoutes, publicRoutes, RouteType } from './router';
+import DefaultLayout from './layout/DefaultLayout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  socketSelector,
+  socketStatusSelector,
+  userSelector,
+} from './redux/selector';
 import Login from './pages/Login';
-import {AppDispatch, RootState} from './redux/store';
+import { AppDispatch, RootState } from './redux/store';
 import { socketConnect, socketSendMessage } from './redux/socketSlice';
 import { SocketEvent } from './model/SocketEvent';
 
-
 function App() {
-    
-    const token = localStorage.getItem('token') ?? '';
-    const userName = localStorage.getItem('userName') ?? '';
-    const user = useSelector(userSelector);
-    const dispatch = useDispatch<AppDispatch>();
-    // const socket = useSelector(socketSelector)
-    const socket: WebSocket | null = useSelector((state: RootState) => state.app.socket.socket);
+  const token = localStorage.getItem('token') ?? '';
+  const userName = localStorage.getItem('userName') ?? '';
+  const user = useSelector(userSelector);
+  const dispatch = useDispatch<AppDispatch>();
+  // const socket = useSelector(socketSelector)
+  const socket = useSelector(socketSelector);
+  const statusSocket = useSelector(socketStatusSelector);
 
-    useLayoutEffect(()=>{
-        console.log("Trying to connect to a websocket...")
-        dispatch(socketConnect(null));
-    },[])
+  useLayoutEffect(() => {
+    console.log('Trying to connect to a websocket...');
+    dispatch(socketConnect(null));
+  }, []);
 
-    useLayoutEffect(()=> {
-        if (socket) {
-            console.log("Checking for reLogin")
-            socket.onopen = ()=> {
-                if (token && userName && !user.username) {
-                    const reloginParams :SocketEvent = {
-                        "action": "onchat",
-                        "data": {
-                            "event": "RE_LOGIN",
-                            "data": {
-                                "user": userName,
-                                "code": token
-                            }
-                        }
-                    }
-                    dispatch(socketSendMessage(reloginParams))
-                }
-            }
-        }
-
-    },[socket])
-    const RouteRender = (route: RouteType, index: number) => {
-        let Layout = route.layout || DefaultLayout
-        let Page = route.element
-        let ChildrenNode = route.child
-        return (
-            <Route key={index} path={route.path} element={
-                <Layout>
-                    <Page/>
-                </Layout>
-            }>
-                {
-                    ChildrenNode.map((routeObject, index: number) =>
-                        RouteRender(routeObject, index)
-                    )
-                }
-            </Route>
-        )
-    };
+  useEffect(() => {
+    if (socket && statusSocket == 'open') {
+      if (token && userName && !user.username) {
+        const reloginParams: SocketEvent = {
+          action: 'onchat',
+          data: {
+            event: 'RE_LOGIN',
+            data: {
+              user: userName,
+              code: token,
+            },
+          },
+        };
+        dispatch(socketSendMessage(reloginParams));
+      }
+    }
+  }, [statusSocket, socket]);
+  const RouteRender = (route: RouteType, index: number) => {
+    let Layout = route.layout || DefaultLayout;
+    let Page = route.element;
+    let ChildrenNode = route.child;
     return (
-        <div>
-            <Router>
-                <Routes>
-                    {
-                        publicRoutes.map((routeObject, index: number) =>
-                            RouteRender(routeObject, index)
-                        )
-                    }
-                    {
-                        token && userName &&
-                        privateRoutes.map((routeObject, index: number) =>
-                            RouteRender(routeObject, index)
-                        )
-                    }
-                    <Route path='*' element={<Login/>}/>
-                </Routes>
-            </Router>
-        </div>
+      <Route
+        key={index}
+        path={route.path}
+        element={
+          <Layout>
+            <Page />
+          </Layout>
+        }
+      >
+        {ChildrenNode.map((routeObject, index: number) =>
+          RouteRender(routeObject, index)
+        )}
+      </Route>
     );
+  };
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {publicRoutes.map((routeObject, index: number) =>
+            RouteRender(routeObject, index)
+          )}
+          {user.username &&
+            privateRoutes.map((routeObject, index: number) =>
+              RouteRender(routeObject, index)
+            )}
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/component/sidebar/Sidebar.tsx
+++ src/pages/component/sidebar/Sidebar.tsx
@@ -1,85 +1,81 @@
-import React, {useEffect, useState} from 'react'
-import {useSelector} from "react-redux";
-import {socketSelector, userSelector} from '~/redux/selector';
-import {UserState} from '~/redux/userSlice';
-import {SideBarItem} from "~/pages/component/sidebar";
-import NavSideBar from "~/pages/component/NavSideBar";
-import {SocketEvent} from "~/model/SocketEvent";
-import {SideBarProp} from "~/model/SideBarProp";
-import {RootState} from "~/redux/store";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  socketSelector,
+  socketStatusSelector,
+  userSelector,
+} from '~/redux/selector';
+import { UserState } from '~/redux/userSlice';
+import { SideBarItem } from '~/pages/component/sidebar';
+import NavSideBar from '~/pages/component/NavSideBar';
+import { SocketEvent } from '~/model/SocketEvent';
+import { SideBarProp } from '~/model/SideBarProp';
+import { AppDispatch, RootState } from '~/redux/store';
+import { socketSendMessage } from '~/redux/socketSlice';
 
 const Sidebar = () => {
-    const userName = localStorage.getItem('userName');
-    const user: UserState = useSelector(userSelector);
-    const [allUsers, setAllUsers] = useState<SideBarProp[]>([]);
-    // const socket = useSelector(socketSelector);
-    const socket: WebSocket | null = useSelector((state: RootState) => state.app.socket.socket);
+  const user: UserState = useSelector(userSelector);
+  const userName = user.username;
+  const [allUsers, setAllUsers] = useState<SideBarProp[]>([]);
+  // const socket = useSelector(socketSelector);
+  const socket = useSelector(socketSelector);
+  const statusSocket = useSelector(socketStatusSelector);
+  const dispatch = useDispatch<AppDispatch>();
+  const getUserParams: SocketEvent = {
+    action: 'onchat',
+    data: {
+      event: 'GET_USER_LIST',
+    },
+  };
 
-    const getUserParams: SocketEvent = {
-        action: 'onchat',
-        data: {
-            event: "GET_USER_LIST",
+  useEffect(() => {
+    if (socket && statusSocket == 'open' && userName) {
+      dispatch(socketSendMessage(getUserParams));
+      socket.onmessage = (event: MessageEvent) => {
+        const data = JSON.parse(event.data);
+        if (data.event === 'GET_USER_LIST' && data.status === 'success') {
+          const conversationUserData = data.data.filter((conv: any) => {
+            if (conv.name != userName) {
+              let sideBarProp: SideBarProp = {
+                type: conv.type,
+                name: conv.name,
+                unseen: false,
+                actionTime: new Date(),
+              };
+              return sideBarProp;
+            }
+          });
+          console.log('conversationUserData', conversationUserData);
+          setAllUsers(conversationUserData);
         }
+      };
     }
+  }, [socket, statusSocket, userName]);
 
-    useEffect(() => {
-        if (socket) {
-            if (socket.readyState === WebSocket.OPEN) {
-                socket.send(JSON.stringify(getUserParams));
-                console.log("socket is sending message")
-            }
+  return (
+    <div className="w-full h-full grid grid-cols-[48px,1fr] bg-white">
+      <NavSideBar name={user.username} />
 
-            socket.onmessage = (event: MessageEvent) => {
-                const data = JSON.parse(event.data);
-                if (data.event === "GET_USER_LIST" && data.status === "success") {
-
-                    const conversationUserData = data.data.filter((conv: any) => {
-                        if (conv.name != userName) {
-                            let sideBarProp: SideBarProp = {
-                                type: conv.type,
-                                name: conv.name,
-                                unseen: false,
-                                actionTime: new Date()
-                            }
-                            return sideBarProp;
-                        }
-                    });
-                    console.log('conversationUserData',conversationUserData);
-                    setAllUsers(conversationUserData);
-                }
-            }
-        } else
-            console.log("socket is not connected");
-        // eslint-disable-next-line
-    }, [socket]);
-
-
-    return (
-        <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
-            <NavSideBar name={user.username}/>
-
-            <div className='w-full'>
-                <div className='h-16 flex items-center py-0.5'>
-                    <h2 className='text-xl font-bold p-4 text-slate-800 '>Message</h2>
-                </div>
-                <div className='bg-slate-200 p-[0.5px] mt-1'></div>
-
-                <div className='h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar'>
-                    {
-                        allUsers.length === 0 && (
-                            <p className='text-lg text-center text-slate-400 pt-5'>Explore users to start a conversation
-                                with.</p>
-                        )
-                    }
-
-                    {
-                        allUsers
-                            .map((user, index) => <SideBarItem key={index} {...user} />)
-                    }
-                </div>
-            </div>
+      <div className="w-full">
+        <div className="h-16 flex items-center py-0.5">
+          <h2 className="text-xl font-bold p-4 text-slate-800 ">Message</h2>
         </div>
-    )
-}
+        <div className="bg-slate-200 p-[0.5px] mt-1"></div>
+
+        <div className="h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar">
+          {allUsers.length === 0 && (
+            <p className="text-lg text-center text-slate-400 pt-5">
+              Explore users to start a conversation with.
+            </p>
+          )}
+
+          {allUsers.map((user, index) => (
+            <SideBarItem key={index} {...user} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
