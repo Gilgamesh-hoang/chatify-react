0d44ab76013167bfef5695025942e4e5ac3b9f87
cuong-do-viet <21130299@st.hcmuaf.edu.vn>
2024-07-10 15:35:19 +0700

    fix: typo1

--- src/pages/component/chatbox/ChatUserMenu.tsx
+++ src/pages/component/chatbox/ChatUserMenu.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react';
+import React from 'react';
 import { IoChatbubbleEllipses, IoInformationCircle } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -21,12 +21,12 @@ const ChatUserMenu: React.FC<{
   const chatUserExist = chatUserList.indexOf(userName) >= 0;
   const navigate = useNavigate();
 
-  const handleInfoClick = (evt: MouseEvent) => {
+  const handleInfoClick = () => {
     onClose();
     onInfoClick();
   };
 
-  const handleSendClick = (evt: MouseEvent) => {
+  const handleSendClick = () => {
     onClose();
     if (chatUserExist)
       navigate(`/0/${userName}`);
@@ -39,12 +39,12 @@ const ChatUserMenu: React.FC<{
       <span className="pl-1">{userName}</span>
       <hr className="my-1" />
       <button className="flex items-center mx-1 p-1 px-2 hover:bg-secondary" onClick={handleInfoClick}>
-        <IoInformationCircle /><span className="pl-1">Check user infomation</span></button>
+        <IoInformationCircle /><span className="pl-1">Check user information</span></button>
 
       <button className="flex items-center mx-1 p-1 px-2 hover:bg-secondary" onClick={handleSendClick}>
         {chatUserExist
-          ? (<><IoChatbubbleEllipses /><span className="pl-1">Open chat messages</span></>)
-          : (<><FaUserPlus /><span className="pl-1">Add users</span></>)}
+          ? (<><IoChatbubbleEllipses /><span className="pl-1">Open chat message</span></>)
+          : (<><FaUserPlus /><span className="pl-1">Add user</span></>)}
 
       </button>
 
